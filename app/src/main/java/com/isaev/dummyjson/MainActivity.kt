077509f14ff962b5d7389338a3e6db6c7f97a02f
Djamb <djambulat69@gmail.com>
2024-03-09 13:58:42 +0300

    LazyList -> LazyGrid

--- app/src/main/java/com/isaev/dummyjson/MainActivity.kt
+++ app/src/main/java/com/isaev/dummyjson/MainActivity.kt
@@ -1,23 +1,23 @@
 package com.isaev.dummyjson
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -27,18 +27,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.IntState
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -46,10 +41,6 @@ import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.isaev.dummyjson.ui.theme.DummyJsonTheme
 import com.isaev.dummyjson.ui.theme.MainViewModel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 
 private const val IMAGES_CHANGE_LAUNCH_KEY = 0
 
@@ -67,10 +58,12 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Column {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
                         MainAppBar()
                         HorizontalDivider()
-                        ProductList(productsState.value)
+                        ProductGrid(products = productsState.value)
                     }
                 }
             }
@@ -84,94 +77,88 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun ProductList(products: List<Product>) {
+    fun ProductGrid(products: List<Product>) {
+        val gridState = rememberLazyGridState()
+        val firstItemIndexState = remember { derivedStateOf { gridState.firstVisibleItemIndex } }
 
-        val listState = rememberLazyListState()
         val pagingState = viewModel.pagingState.observeAsState()
 
-        val firstItemIndexState = remember { derivedStateOf { listState.firstVisibleItemIndex } }
+        Column {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                contentPadding = PaddingValues(horizontal = 4.dp),
+                state = gridState
+            ) {
+                val productType = 0
+                items(products, contentType = { productType }) { product ->
+                    ProductGridItem(product)
+                }
 
-        val photoIndex = rememberSaveable { mutableIntStateOf(0) }
-        val MAX_IMAGES_COUNT = 10
-
-        LaunchedEffect(IMAGES_CHANGE_LAUNCH_KEY) {
-            launch {
-                while (true) {
-                    delay(5000)
-                    photoIndex.intValue = (photoIndex.intValue + 1) % MAX_IMAGES_COUNT
+                item(span = { GridItemSpan(maxLineSpan) }) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        if (pagingState.value == DataState.LOADING) {
+                            CircularProgressIndicator(modifier = Modifier.size(60.dp))
+                        } else if (pagingState.value == DataState.FAILURE) {
+                            ErrorMessage(onRetry = { viewModel.getMoreProducts() })
+                        }
+                    }
                 }
             }
-        }
 
-        LazyColumn(
-            state = listState, horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            items(products) {
-                ProductItem(product = it, photoIndex)
-            }
-
-            if (pagingState.value == DataState.LOADING) {
-                item {
-                    CircularProgressIndicator()
+            if (firstItemIndexState.value >= products.size - 6 && pagingState.value == DataState.SUCCESS) {
+                // Чтобы не было бесконечной загрузки в конце (Я так понял в бэкенде 100 элементов максимум)
+                if (products.size < 100) {
+                    viewModel.getMoreProducts()
                 }
-            } else if (pagingState.value == DataState.FAILURE) {
-                item {
-                    ErrorMessage(onRetry = { viewModel.getMoreProducts() })
-                }
-            }
-        }
-
-        if (firstItemIndexState.value >= products.size - 10 && pagingState.value == DataState.SUCCESS) {
-            // Чтобы не было бесконечной загрузки в конце (Я так понял в бэкенде 100 элементов максимум)
-            if (products.size < 100) {
-                viewModel.getMoreProducts()
             }
         }
     }
 
     @Composable
-    fun ProductItem(product: Product, photoIndex: IntState) {
-
-        Row(
+    fun ProductGridItem(product: Product) {
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 2.dp, horizontal = 6.dp)
-                .wrapContentHeight()
-                .clickable {
-                    startActivity(Intent(this, ProductActivity::class.java).apply {
-                        putExtra(
-                            ProductActivity.PRODUCT_EXTRA_KEY, Json.encodeToString(product)
-                        )
-                    })
-                },
+                .padding(4.dp)
+                .height(360.dp)
         ) {
             AsyncImage(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(product.images[photoIndex.intValue % product.images.size])
-                    .transitionFactory(
-                        CrossfadeTransitionFactoryFixed()
-                    ).build(),
+                model = ImageRequest.Builder(this@MainActivity).data(product.thumbnail)
+                    .crossfade(true).build(),
                 contentScale = ContentScale.Crop,
                 contentDescription = product.title,
                 modifier = Modifier
-                    .size(120.dp)
-                    .clip(RoundedCornerShape(4.dp))
+                    .fillMaxWidth()
+                    .height(220.dp)
             )
-            Spacer(modifier = Modifier.width(10.dp))
-            Column {
-                Text(
-                    text = product.title,
-                    style = MaterialTheme.typography.titleLarge,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis
-                )
-                Text(
-                    text = product.description,
-                    style = MaterialTheme.typography.bodyMedium,
-                    maxLines = 5,
-                    overflow = TextOverflow.Ellipsis
-                )
-            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Text(
+                text = "${product.price} $",
+                style = MaterialTheme.typography.titleLarge,
+                maxLines = 1,
+                color = Color.White,
+                modifier = Modifier
+                    .background(
+                        color = MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(4.dp)
+                    )
+                    .padding(horizontal = 4.dp, vertical = 2.dp)
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = product.title,
+                style = MaterialTheme.typography.titleMedium,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = product.description,
+                style = MaterialTheme.typography.bodyMedium,
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis
+            )
         }
     }
 }
